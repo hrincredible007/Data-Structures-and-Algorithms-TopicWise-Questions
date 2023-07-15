523fad2f9021e7e4d274df1545000679dbf2215c
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-07-15 06:25:44 +0530

    Two-Pointers

---
+++ src/TwoPointer/Main.java
@@ -0,0 +1,12 @@
+package TwoPointer;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args0){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+
+}
