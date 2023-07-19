b4ef3aff6d862546a1849d1cc99e6f0a05b8ee26
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-07-19 12:27:27 +0530

    LC:992

--- src/Sliding_Window/LC_992_Subarrays_with_K_Different_Integers.java
+++ src/Sliding_Window/LC_992_Subarrays_with_K_Different_Integers.java
@@ -1,5 +1,29 @@
 package Sliding_Window;
-
+import java.util.*;
 public class LC_992_Subarrays_with_K_Different_Integers {
-
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return sol(nums, k) - sol(nums, k-1);
+    }
+    public int sol(int[] nums, int k){
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        while(j < n){
+            hashMap.put(nums[j], hashMap.getOrDefault(nums[j], 0)+1);
+            while(hashMap.size() > k){
+                if(hashMap.get(nums[i]) == 1){
+                    hashMap.remove(nums[i]);
+                }
+                else{
+                    hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0)-1);
+                }
+                i++;
+            }
+            count+= j-i+1;
+            j++;
+        }
+        return count;
+    }
 }
