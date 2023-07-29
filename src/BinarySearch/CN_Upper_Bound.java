b20f3eecc602ac1ad9008c38b279c65844714488
hrincredible007 <hritikbhardwaj2002@gmail.com>
2023-07-29 17:08:14 +0530

    CN:Upper_Bound

---
+++ src/BinarySearch/CN_Upper_Bound.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class CN_Upper_Bound {
+    public static int lowerBound(int[] nums, int n, int x) {
+        // Write your code here
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = low+ (high- low)/2;
+            if(nums[mid] > x){
+                ans = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
+
+}
